4dee50527b79ed1d4647c060aa8693e781020efb
accdnegel <anabezac99@gmail.com>
2026-09-23 21:20:46 +0800

    Graphics to Geometry

--- app/src/main/java/com/pitaka/app/ui/components/Shared.kt
+++ app/src/main/java/com/pitaka/app/ui/components/Shared.kt
@@ -23,9 +23,9 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.graphics.Offset
+import androidx.compose.ui.geometry.Offset
 import androidx.compose.ui.graphics.Path
-import androidx.compose.ui.graphics.Rect
+import androidx.compose.ui.geometry.Rect
 import androidx.compose.ui.graphics.drawscope.Stroke
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.unit.dp
