9824cf70df0b060e91707d75af59a5d20038bdea
accdnegel <anabezac99@gmail.com>
2026-09-24 21:15:07 +0800

    test: add Room testing dependency

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -41,6 +41,7 @@ android {
 
 dependencies {
     testImplementation("junit:junit:4.13.2")
+    testImplementation("androidx.room:room-testing:2.6.1")
     implementation("androidx.core:core-ktx:1.13.1")
     implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.8.4")
     implementation("androidx.lifecycle:lifecycle-viewmodel-compose:2.8.4")
